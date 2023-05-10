e0398ad22b1ca2d7be45fcc0cebcd5fdafae3033
Trois25 <89327360+Trois25@users.noreply.github.com>
2023-05-10 21:12:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("valo").onclick = function () {
+    location.href = "https://playvalorant.com/id-id/";
+};
+
+document.getElementById("hsr").onclick = function () {
+    location.href = "https://hsr.hoyoverse.com/en-us/";
+};
+
+document.getElementById("ml").onclick = function () {
+    location.href = "https://m.mobilelegends.com/id";
+};
+
